6d489ac6f1632242d81f7ed615c52c19571278d3
mae-tin <ceclass7@yahoo.co.jp>
2020-12-07 20:41:49 +0900

    Rubocop実行

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
+
   validates :nickname, :birthday, presence: true
-  validates :password, 
-            format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i,message: "Include both letters and numbers" }
+  validates :password,
+            format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: 'Include both letters and numbers' }
   validates :last_name, :first_name, presence: true,
-            format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "Full-width characters" }
+                                     format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'Full-width characters' }
   validates :last_name_kana, :first_name_kana, presence: true,
-            format: { with: /\A[ァ-ヶー－]+\z/, message: "Full-width katakana characters" }
+                                               format: { with: /\A[ァ-ヶー－]+\z/, message: 'Full-width katakana characters' }
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -8,7 +8,7 @@ FactoryBot.define do
     last_name { Faker::Japanese::Name.last_name }
     first_name { Faker::Japanese::Name.first_name }
     last_name_kana { Faker::Japanese::Name.last_name.yomi }
-    first_name_kana{  Faker::Japanese::Name.first_name.yomi }
+    first_name_kana { Faker::Japanese::Name.first_name.yomi }
     birthday { Faker::Date.between(from: '1930-01-01', to: '2020-12-31') }
   end
 end
