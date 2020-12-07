b62a35b7c862b071e08b62e671a64f758b9cc3ef
mae-tin <ceclass7@yahoo.co.jp>
2020-12-07 11:41:24 +0900

    Userテーブルバリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  validates :encypted_password, :nickname, :last_name, :first_name,
+            :last_name_kana, :first_name_kana, :birthday, presence: true
+  validates :email, presence: true, uniqueness: true
+
 end
