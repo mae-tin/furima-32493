ad44d69cd7c5cb42ed65f8ca66018f6cf5af18ef
mae-tin <ceclass7@yahoo.co.jp>
2020-12-08 11:09:43 +0900

    画像系機能インストール
    itemモデルとのアソシエーションまで

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,7 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category, :condition, :delivery_fee, :prefecture, :day
   belongs_to :user
+  has_one_attached :image
 
   validates :name, :text, :condition_id, :derivery_fee_id,
             :prefecture_id, :days_id, :price, :user, presence: true 
