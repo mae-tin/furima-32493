6a6bf138e05f21260093e3e24396dd31318e79f7
mae-tin <ceclass7@yahoo.co.jp>
2020-12-08 19:23:18 +0900

    商品出品機能修正（2回目）
    itemモデルバリデーション書き方修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,11 +4,19 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
-  validates :image, :text, :user, presence: true
-  validates :name, length: { maximum: 40 }, presence: true
-  validates :text, length: { maximum: 1000 }, presence: true
-  validates :price, format: { with: /\A(?=.*?\d)\d+\z/i, message: 'Half-width number' },
-                    numericality: { greater_than: 299, less_than: 9_999_999, message: 'Out of setting range' }, presence: true
-  validates :category_id, :condition_id, :delivery_fee_id, :prefecture_id, :days_id,
-            numericality: { other_than: 1, message: 'select' }
+  with_options presence: true do
+    validates :image, :text, :user
+    validates :name, length: { maximum: 40 }
+    validates :text, length: { maximum: 1000 }
+    validates :price, format: { with: /\A(?=.*?\d)\d+\z/i, message: 'Half-width number' },
+                      numericality: { greater_than: 299, less_than: 9999999, message: 'Out of setting range' }                  
+    with_options numericality: { other_than: 1, message: 'select' } do
+      validates :category_id
+      validates :condition_id
+      validates :delivery_fee_id
+      validates :prefecture_id
+      validates :days_id
+    end
+  end  
 end
+
