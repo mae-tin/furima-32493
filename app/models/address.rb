7f291ad3ff93380bbd37a132e5fda908d61792d2
mae-tin <ceclass7@yahoo.co.jp>
2020-12-11 18:16:06 +0900

    商品購入機能修正（2回目）
    Addressモデルバリデーション削除

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,11 +1,3 @@
 class Address < ApplicationRecord
   belongs_to :order
-
-  with_options presence: true do
-    validates :postcode, format: { with: /\A\d{3}-\d{4}\z/, message: 'Half-width number' }
-    validates :prefecture_id
-    validates :city
-    validates :block
-    validates :phone_number, format: { with: /\A\d{11}\z/, message: 'Half-width number' }
-  end
 end
