709c6ae6765a1055a3c84718385f9e1406e8b5b2
mae-tin <ceclass7@yahoo.co.jp>
2020-12-11 11:32:28 +0900

    商品購入機能実装2
    商品購入機能実装
    （決済機能）

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,13 +4,13 @@ class OrdersController < ApplicationController
   before_action :redirect_check, only: [:index]
   
   def index
-    binding.pry
     @order_address = OrderAddress.new
   end
 
   def create
     @order_address = OrderAddress.new(order_address_params)
     if @order_address.valid?
+      pay_item
       @order_address.save
       redirect_to root_path
     else
@@ -31,6 +31,15 @@ class OrdersController < ApplicationController
   end
 
   def order_address_params
-    params.require(:order_address).permit(:postcode, :prefecture_id, :city, :block, :building, :phone_number, :item_id).merge(user_id: current_user.id,item_id: params[:item_id])
+    params.require(:order_address).permit(:postcode, :prefecture_id, :city, :block, :building, :phone_number, :item_id).merge(user_id: current_user.id,item_id: params[:item_id],token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: order_address_params[:token],
+      currency: 'jpy'
+    )
   end
 end
--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,13 +1,14 @@
 class OrderAddress
   include ActiveModel::Model
-  attr_accessor :postcode, :prefecture_id, :city, :block, :building, :phone_number, :item_id, :user_id
+  attr_accessor :postcode, :prefecture_id, :city, :block, :building, :phone_number, :item_id, :user_id, :token
 
   with_options presence: true do
+    validates :token
     validates :postcode, format: {with: /\A\d{3}[-]\d{4}\z/, message: "Input correctly"}
+    validates :prefecture_id, numericality: { other_than: 1, message: 'select' }
     validates :city
     validates :block
     validates :phone_number , format: {with: /\A\d{11}\z/, message: "Input correctly"}
-    validates :prefecture_id, numericality: { other_than: 1, message: 'select' }
     validates :user_id
     validates :item_id
   end
