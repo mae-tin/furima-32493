f74afa195682906e376c73578d3b4f0e282632fd
mae-tin <ceclass7@yahoo.co.jp>
2020-12-15 16:05:50 +0900

    githubユーザー名変更

--- config/deploy.rb
+++ config/deploy.rb
@@ -40,7 +40,7 @@ set :repo_url, "git@example.com:me/my_repo.git"
 
 lock '3.14.1'
 set :application, 'furima-32493'
-set :repo_url, 'git@github.com:mae-tin/furima-32493.git'
+set :repo_url, 'git@github.com:maeda-300/furima-32493.git'
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 set :rbenv_type, :user
 set :rbenv_ruby, '2.6.5'
